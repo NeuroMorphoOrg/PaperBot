5ff06db2f4e2b901da61c689fb4f52d55e1dd8a2
pmaraver <pmaraver@mbp-de-patricia.krasnow.gmu.edu>
2018-02-02 16:49:00 -0500

    bug corrected

--- LiteratureDownloadPDFService/src/main/java/org/neuromorpho/literature/download/communication/LiteratureCommunication.java
+++ LiteratureDownloadPDFService/src/main/java/org/neuromorpho/literature/download/communication/LiteratureCommunication.java
@@ -18,7 +18,9 @@ public class LiteratureCommunication {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     public ArticlePage getArticleList(Integer page, String status) {
-        String url = uri + "/query?collection=" + status + "&page=" + page;
+        
+        String url = uri + "/query?collection=" + status + "&page=" + page + 
+                "&pdfPath=$exist:false&doi=$exist:true";
         log.debug("Creating rest connection for URI: " + url);
         RestTemplate restTemplate = new RestTemplate();
         ArticlePage articlePage = restTemplate.getForObject(url, ArticlePage.class);
@@ -28,7 +30,7 @@ public class LiteratureCommunication {
     
      public void updateArticlePdf(String id, String downlodedPath) {
         Map<String, Object> article = new HashMap();
-        article.put("PDFPath", downlodedPath);
+        article.put("pdfPath", downlodedPath);
         String url = uri + "/update/" + id;
         log.debug("Creating rest connection for URI: " + url);
         RestTemplate restTemplate = new RestTemplate();
