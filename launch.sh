376813678bd5306428ebb60b9faa745d481982a6
pmaraver <pmaraver@mbp-de-patricia.krasnow.gmu.edu>
2018-02-13 16:18:22 -0500

    new launch file

---
+++ launch.sh
@@ -0,0 +1,6 @@
+nohup java -jar LiteratureMetadataServiceBoot/target/LiteratureMetadataServiceBoot-1.0.jar &
+nohup java -jar LiteraturePortalServiceBoot/target/LiteraturePortalServiceBoot-1.0.jar &
+nohup java -jar LiteraturePubMedServiceBoot/target/LiteraturePubMedServiceBoot-1.0.jar &
+nohup java -jar LiteratureServiceBoot/target/LiteratureServiceBoot-1.0.jar &
+nohup java -jar LiteratureCrossRefServiceBoot/target/LiteratureCrossRefServiceBoot-1.0.jar &
+
