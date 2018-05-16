5c13f6ac352788a221624ca272219f3e54a770c2
pmaraver <pmaraver@mbp-de-patricia.krasnow.gmu.edu>
2018-05-16 00:31:08 -0400

    Wiley 404 workaround

--- CrossRef/src/main/java/org/paperbot/crossref/service/CrossRefService.java
+++ CrossRef/src/main/java/org/paperbot/crossref/service/CrossRefService.java
@@ -3,14 +3,21 @@ package org.paperbot.crossref.service;
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSFile;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.paperbot.crossref.communication.CrossRefConnection;
 import org.paperbot.crossref.model.Article;
 import org.paperbot.crossref.model.Author;
@@ -87,80 +94,29 @@ public class CrossRefService {
     }
 
     public Boolean downloadPDFFromDOI(String doi, String id, Boolean download) throws Exception {
-        Map message = crossRefConnection.findMetadataFromDOI(doi);
-        ArrayList<Map> links = (ArrayList) message.get("link");
-        GridFSFile file = null;
-        String idPdf = null;
+        GridFSDBFile dbfile = this.findPDF(id);
         Boolean exists = Boolean.FALSE;
-        if (links != null) {
-            for (Map link : links) {
-                String contentType = (String) link.get("content-type");
-                if (contentType.equals("application/pdf")
-                        || contentType.equals("unspecified")) {
-                    try {
-
-                        //Download pdf
-                        RestTemplateTimeout restTemplate = new RestTemplateTimeout();
+        if (dbfile == null) { // if is not already in DB download
+            Map message = crossRefConnection.findMetadataFromDOI(doi);
+            ArrayList<Map> links = (ArrayList) message.get("link");
+            if (links != null) {
+                for (Map link : links) {
+                    String contentType = (String) link.get("content-type");
+                    if (contentType.equals("application/pdf")
+                            || contentType.equals("unspecified")) {
                         String url = (String) link.get("URL");
-                        log.debug("Accesing download pdf: " + url);
+                        exists = this.downloadPDF(url, download, id, null);
 
-                        HttpHeaders headersResponse = restTemplate.headForHeaders(url);
-
-                        log.debug("Content type fo the url: " + headersResponse.getContentType()
-                                + " location :" + headersResponse.getLocation());
-
-                        while (!headersResponse.getContentType().toString().equals(MediaType.APPLICATION_PDF_VALUE)
-                                && headersResponse.getLocation() != null) {
-                            log.debug("Content type fo the url: " + headersResponse.getContentType()
-                                    + " location :" + headersResponse.getLocation());
-                            headersResponse = restTemplate.headForHeaders(headersResponse.getLocation());
-//                       
-//                        
-
-//                        while (response.getStatusCode() == HttpStatus.SEE_OTHER
-//                                || response.getStatusCode() == HttpStatus.FOUND
-//                                || response.getStatusCode() == HttpStatus.MOVED_PERMANENTLY) {
-//                            response = restTemplate.exchange(
-//                                    response.getHeaders().getLocation(), HttpMethod.GET, entity, byte[].class);
-//                        }
-                        }
-
-                        if (headersResponse.getContentType().toString().equals(MediaType.APPLICATION_PDF_VALUE)) {
-                            log.debug("PDF found ");
-                            exists = true;
-                            if (download) {
-                                log.debug("Downalong file");
-                                GridFSDBFile dbfile = this.findPDF(id);
-                                if (dbfile == null) { // if is not already in DB download
-                                    restTemplate.getMessageConverters().add(
-                                            new ByteArrayHttpMessageConverter());
-
-                                    HttpHeaders headers = new HttpHeaders();
-                                    headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
-                                    headers.set("CR-Clickthrough-Client-Token", token);
-
-                                    HttpEntity<String> entity = new HttpEntity<>(headers);
-                                    ResponseEntity<byte[]> response = restTemplate.exchange(
-                                            url, HttpMethod.GET, entity, byte[].class);
-                                    if (response.getStatusCode() == HttpStatus.OK) {
-                                        byte[] pdf = response.getBody();
-                                        file = gridOperations.store(new ByteArrayInputStream(pdf), id);
-                                        if (file != null) {
-                                            idPdf = file.getId().toString();
-                                        }
-                                    }
-
-                                }
-                            }
-                        }
-                    } catch (Exception ex) {
-                        log.debug("CrossRef link not working, trying other links");
                     }
-
-                } else {
-                    log.warn("Article in CrossRef but no pdf associated");
                 }
             }
+            String url = (String) message.get("URL");
+            if (url != null && !exists) {
+                exists = this.extract(url, download, id);
+            }
+        } else {
+            exists = Boolean.TRUE;
+            log.debug("PDF downloaded in previous searches");
         }
         return exists;
     }
@@ -184,17 +140,94 @@ public class CrossRefService {
         }
         return null;
     }
-    
+
     public class RestTemplateTimeout extends RestTemplate {
-    public RestTemplateTimeout() {
-        if (getRequestFactory() instanceof SimpleClientHttpRequestFactory) {
-            ((SimpleClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(10 * 1000);
-            ((SimpleClientHttpRequestFactory) getRequestFactory()).setReadTimeout(10 * 1000);
-        } else if (getRequestFactory() instanceof HttpComponentsClientHttpRequestFactory) {
-            ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setReadTimeout(10 * 1000);
-            ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(10 * 1000);
+
+        public RestTemplateTimeout() {
+            if (getRequestFactory() instanceof SimpleClientHttpRequestFactory) {
+                ((SimpleClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(10 * 1000);
+                ((SimpleClientHttpRequestFactory) getRequestFactory()).setReadTimeout(10 * 1000);
+            } else if (getRequestFactory() instanceof HttpComponentsClientHttpRequestFactory) {
+                ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setReadTimeout(10 * 1000);
+                ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(10 * 1000);
+            }
         }
     }
-}
+
+    private Boolean extract(String url, Boolean download, String id) throws IOException {
+        Response res = Jsoup.connect(url)
+                .timeout(60 * 1000)
+                .followRedirects(true)
+                .header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+                .execute();
+
+        Document doc = res.parse();
+        Map<String, String> cookies = res.cookies();
+        Element element = doc.select("meta[name=citation_pdf_url]").first();
+        return this.downloadPDF(element.attr("content"), download, id, cookies);
+
+    }
+
+    private Boolean downloadPDF(String url, Boolean download, String id, Map<String, String> cookies) {
+        Boolean exists = Boolean.FALSE;
+        try {
+
+            //Download pdf
+            RestTemplateTimeout restTemplate = new RestTemplateTimeout();
+            log.debug("Accesing download pdf: " + url);
+            restTemplate.getMessageConverters().add(
+                    new ByteArrayHttpMessageConverter());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+            headers.set("CR-Clickthrough-Client-Token", token);
+            if (cookies != null) {
+                for (Map.Entry<String, String> entry : cookies.entrySet()) {
+                    headers.add("Cookie", entry.getKey() + "=" + entry.getValue());
+
+                }
+            }
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            ResponseEntity<byte[]> response = restTemplate.exchange(
+                    url, HttpMethod.GET, entity, byte[].class);
+
+            log.debug("Content type for the url: " + response.getHeaders().getContentType()
+                    + " location :" + response.getHeaders().getLocation());
+
+            while (response.getStatusCode() == HttpStatus.SEE_OTHER
+                    || response.getStatusCode() == HttpStatus.FOUND
+                    || response.getStatusCode() == HttpStatus.MOVED_PERMANENTLY) {
+                log.debug("Content type fo the url: " + response.getHeaders().getContentType()
+                        + " location :" + response.getHeaders().getLocation());
+                response = restTemplate.exchange(response.getHeaders().getLocation(), HttpMethod.GET, entity, byte[].class);
+
+            }
+
+            if (response.getHeaders().getContentType().toString().contains(MediaType.APPLICATION_PDF_VALUE.toString())
+                    || response.getHeaders().getContentType().toString().contains(MediaType.APPLICATION_OCTET_STREAM.toString())) {
+                log.debug("PDF found ");
+                exists = Boolean.TRUE;
+                if (download) {
+                    log.debug("Downloading file");
+
+                    if (response.getStatusCode() == HttpStatus.OK) {
+                        byte[] pdf = response.getBody();
+                        GridFSFile file = gridOperations.store(new ByteArrayInputStream(pdf), id);
+                        if (file != null) {
+                            String idPdf = file.getId().toString();
+                        }
+                    }
+
+                }
+            }
+
+        } catch (Exception ex) {
+            log.debug("CrossRef link not working, trying other links");
+            log.error("Error:", ex);
+        }
+        return exists;
+
+    }
 
 }
